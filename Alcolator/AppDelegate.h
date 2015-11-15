a83b7962a033458bfe8ff48e87ac16aae2e03871
Nathan Boosinger <nathan.boosinger@gmail.com>
2015-11-15 09:42:12 -0600

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Nathan Boosinger on 11/15/15.
+//  Copyright (c) 2015 Nathan Boosinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Nathan Boosinger on 11/15/15.
+//  Copyright (c) 2015 Nathan Boosinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
